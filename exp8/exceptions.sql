c575fbba2d69ee054f6f03ad5173ebe0b5e35e5e
Jayaraman <adarsh1999@gmail.com>
2019-03-07 15:33:37 +0530

    Updated exp 8

--- exp8/exceptions.sql
+++ exp8/exceptions.sql
@@ -1,29 +1,30 @@
-rem 1. For the given receipt number, if there are no rows then display as “No order with the
-rem given receipt <number>”. If the receipt contains more than one item, display as
-rem “The given receipt <number> contains more than one item”. If the receipt contains
-rem single item, display as “The given receipt <number> contains exactly one item”. Use
+rem 1. For the given receipt number, if there are no rows then display as No order with the
+rem given receipt <number>. If the receipt contains more than one item, display as
+rem The given receipt <number> contains more than one item. If the receipt contains
+rem single item, display as The given receipt <number> contains exactly one item. Use
 rem predefined exception handling.
 declare
   rows item_list%rowtype;
   rnum receipts.rno%type;
-  one_row_exception exception;
+  one_row exception;
 begin
   rnum :=&rno;
   select * into rows
   from item_list
   where rno = rnum;
   if SQL%rowcount =1 then
-    raise one_row_exception;
+    raise one_row;
   end if;
 exception
-  when one_row_exception then
-    dbms_output.put_line('The given receipt '|| to_char(rnum)||' has exactly one item');
   when no_data_found then
     dbms_output.put_line('The given receipt '|| to_char(rnum)||' has no orders');
+  when one_row then
+    dbms_output.put_line('The given receipt '|| to_char(rnum)||' has exactly one item');
   when too_many_rows then
     dbms_output.put_line('The given receipt '|| to_char(rnum)||' contains more than one item');
 end;
 /
+rem Use these rnumbers
 rem 121 17947 21040
 
 rem 2. While inserting the receipt details, raise an exception when the receipt date is greater
